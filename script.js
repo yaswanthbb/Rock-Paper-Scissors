0a5e518e04a2b47ee5c388f4bd379cf9ce305208
Yaswanth <byaswanth2003@gmail.com>
2023-05-13 13:24:25 +0530

    Add No of Rounds - Create game function - Take no of rounds input - Run game function as many times as input
    [Ticket: 03]

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const getComputerChoice = () =>{
 const result = (playerSelection,computerSelection)=>{
     console.log(computerSelection)
     if((playerSelection == 'rock' && computerSelection =='scissors') || (playerSelection == 'paper' && computerSelection =='rock') || (playerSelection == 'scissors' && computerSelection =='paper')){
-        console.log(`You Win ${playerSelection} beats ${computerSelection}`)
+        
     }
     else if (playerSelection === computerSelection){
         console.log("It's a Tie")
@@ -16,6 +16,14 @@ const result = (playerSelection,computerSelection)=>{
         console.log(`You Lose ${computerSelection} beats ${playerSelection}`)
     }
 }
-const playerSelection = (prompt('Select Your Entry : ')).toLowerCase()
-const computerSelection = getComputerChoice()
-result(playerSelection,computerSelection)
+const game = (num)=>{
+    for (let index = 1; index <= num; index++) {
+        const playerSelection = (prompt('Select Your Entry : ')).toLowerCase()
+        const computerSelection = getComputerChoice()
+        result(playerSelection,computerSelection)
+    }
+}
+
+const noOfGames = +prompt('No of Rounds you want play : ')
+
+game(noOfGames)
