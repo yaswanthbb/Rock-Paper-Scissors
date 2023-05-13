3cf5ba169fe33dd3e4c0542c997061c69bd4bb9f
Yaswanth <byaswanth2003@gmail.com>
2023-05-13 14:11:46 +0530

    Add Points System - Create two variables playerPoints and computerPoints - Add Points using if else from result Function - Add if else to show who is winner [Ticket: 06]

--- script.js
+++ script.js
@@ -7,23 +7,40 @@ const getComputerChoice = () =>{
 const result = (playerSelection,computerSelection)=>{
     console.log(computerSelection)
     if((playerSelection == 'rock' && computerSelection =='scissors') || (playerSelection == 'paper' && computerSelection =='rock') || (playerSelection == 'scissors' && computerSelection =='paper')){
-        
+        playerPoints+=1
+        console.log(`You Win ${playerSelection} beats ${computerSelection}`)
     }
     else if (playerSelection === computerSelection){
         console.log("It's a Tie")
     }
     else{
         console.log(`You Lose ${computerSelection} beats ${playerSelection}`)
+        computerPoints+=1
     }
 }
 const game = (num)=>{
     for (let index = 1; index <= num; index++) {
-        const playerSelection = (prompt('Select Your Entry : ')).toLowerCase()
-        const computerSelection = getComputerChoice()
+        let playerSelection = (prompt('Select Your Entry : ')).toLowerCase()
+        let computerSelection = getComputerChoice()
         result(playerSelection,computerSelection)
+    }
+    console.log
+    (`
+    Player Points : ${playerPoints}
+    Computer Points : ${computerPoints}
+    `)
+    if(playerPoints > computerPoints){
+        console.log(`You win`)
+    }
+    else if (playerPoints == computerPoints){
+        console.log(`The Game is a Tie`)
+    }
+    else{
+        console.log('You Lose')
     }
 }
 
 const noOfGames = +prompt('No of Rounds you want play : ')
-
+let playerPoints = 0
+let computerPoints = 0
 game(noOfGames)
